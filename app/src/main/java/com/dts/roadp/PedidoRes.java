63f3bc501fc9754406fc38109fc7cf6c8b607201
dtsolutionsgt <dtsolutionsgt@gmail.com>
2024-07-08 17:33:03 -0600

    jp20240708

--- app/src/main/java/com/dts/roadp/PedidoRes.java
+++ app/src/main/java/com/dts/roadp/PedidoRes.java
@@ -1036,8 +1036,6 @@ public class PedidoRes extends PBase {
 				"AND (ANULADO='N') AND (FECHAENTR="+fechae+")";
 			dt=Con.OpenDT(sql);
 
-
-
 			mt=0;
 			if (dt.getCount()>0) {
 				dt.moveToFirst();
